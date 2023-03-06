c75028d687cc14ee45c1f909cee3b1222b4baac3
abel-tefera <abeltefera16@gmail.com>
2023-03-06 11:06:56 +0300

    Fixed lighthouse error

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-/* eslint-disable */
-
 const showMobileMenu = () => {
-  const menu = document.getElementById("mobile-menu");
-  menu.style.visibility = "visible";
-  menu.style.height = "100%";
-  menu.style.width = "100%";
+  const menu = document.getElementById('mobile-menu');
+  menu.style.visibility = 'visible';
+  menu.style.height = '100%';
+  menu.style.width = '100%';
 };
 
 const closeMobileMenu = () => {
-  const menu = document.getElementById("mobile-menu");
-  menu.style.height = "0";
+  const menu = document.getElementById('mobile-menu');
+  menu.style.height = '0';
   setTimeout(() => {
-    menu.style.visibility = "hidden";
+    menu.style.visibility = 'hidden';
   }, 500);
 };
 
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll(".links");
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.links');
 
 window.onscroll = () => {
   sections.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 150;
+    const height = sec.offsetHeight;
+    const id = sec.getAttribute('id');
 
     if (top >= offset && top < offset + height) {
       navLinks.forEach((links) => {
-        links.classList.remove("active");
+        links.classList.remove('active');
         document
-          .querySelector(".links-container a[href*=" + id + "]")
-          .classList.add("active");
+            .querySelector('.links-container a[href*=' + id + ']')
+            .classList.add('active');
       });
     }
   });
